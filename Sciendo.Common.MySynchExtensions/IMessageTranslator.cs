080731558ea0863ec60ee1aae87892dac13d0292
oduma <tavi_duma2002@yahoo.co.uk>
2017-11-15 09:03:19 +0000

    Added the IMessageTranslator in a nuget package

---
+++ Sciendo.Common.MySynchExtensions/IMessageTranslator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Sciendo.Common.MySynchExtensions
+{
+    public interface IMessageTranslator
+    {
+        byte[] Translate(byte[] inBytes, Dictionary<string, string> findReplacePairs);
+    }
+}
