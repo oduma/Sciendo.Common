adba37d492b2ed3c485a0d5cc0deff57737b7b9a
oduma <tavi_duma2002@yahoo.co.uk>
2017-11-15 23:29:44 +0000

    and the code for it

--- Sciendo.Common.MySynchExtensions/IBinaryMessageTranslator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Sciendo.Common.MySynchExtensions
-{
-    public interface IBinaryMessageTranslator
-    {
-        byte[] Translate(byte[] inBytes, Dictionary<object, object> findReplacePairs);
-    }
-}
